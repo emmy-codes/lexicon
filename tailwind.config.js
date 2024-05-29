14f632f51e27ff5942cf03da3443deae682e794d
Emma <alunia.moon@gmail.com>
2024-05-29 20:19:46 +0200

    Initial commit - Install Tailwind CSS and add 2 components to strip/customize

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'shamrock': {
+        '50': '#eefbf4',
+        '100': '#d5f6e2',
+        '200': '#afebc9',
+        '300': '#7bdaaa',
+        '400': '#4ac48b',
+        '500': '#22a76d',
+        '600': '#148757',
+        '700': '#106c49',
+        '800': '#0f563b',
+        '900': '#0e4631',
+        '950': '#06281d',
+      },
+
+    }
+  },
+  plugins: [],
+}
