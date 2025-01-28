305f2e6d942881383b16d87cfdd482c7924c1864
Emma <alunia.moon@gmail.com>
2025-01-28 15:27:08 +0100

    add more static HTML and fetch console logged data on button click
    Targetting the button once it was rendered was proving too difficult so I changed approach and added the base details in the HTML whilst giving them a dataset to apply the correct fetch link

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const fetchApiData = async () => {
             stat.count = 0;
         });
 
-        console.log(data.results)
+        // console.log(data.results)
         // save in localstorage
         localStorage.setItem("character_stats", JSON.stringify(data));
         return data;
@@ -23,33 +23,57 @@ const fetchApiData = async () => {
 
 };
 
-// render from Local storage to UI
-const renderCharacterStats = (stats) => {
-    console.log("stats", stats);
-    const statsContainer = document.getElementById("stats-info");
-    statsContainer.innerHTML = stats.results.map(stat =>
-        `
-            <article class="${stat.name}">
-                <h2>${stat.name}</h2>
-                    <input type="number">
-                    <button class="save-stat">Save</button>
-                        <button id="${stat.name.toLowerCase()}-info">Read up on this stat</button>
-            </article>
-            `.trim()).join("");
-};
-
 // check if data stored in local storage
 const checkCurrentData = async () => {
     const character_stats = JSON.parse(localStorage.getItem("character_stats"));
     if (character_stats) {
         console.log("There are characters saved");
-        renderCharacterStats(character_stats);
+        // renderCharacterStats(character_stats);
     } else {
         console.log("No characters yet");
         const data = await fetchApiData();
-        renderCharacterStats(data);
+        // console.log(data);
+    };
+};
+
+
+/* 
+add stats to array 
+add max stats
+deduct available stats from max stats when a number of stats is chosen
+
+*/
+
+const applyingStatChoices = async () => {
+    const maxStats = 20;
+
+}
+
+// new API call to use for "read more about this stat" https://www.dnd5eapi.co/api/ability-scores/${url}
+
+const fetchStatDetails = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Failed to fetch stat description");
+        }
+        const data = await response.json();
+        console.log(data.desc)
+    } catch (error) {
+        console.error(error);
     }
 };
+
+// event listener for buttons to fetch description from api
+const statsContainer = document.getElementById("stats-info");
+statsContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+        const url = e.target.dataset.url; // dataset is the data-url tag on the HTML elements
+        fetchStatDetails(`https://www.dnd5eapi.co${url}`)
+    }
+})
+
+
 
 fetchApiData();
 checkCurrentData();
