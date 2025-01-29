bab1ceb171e11ca5dac7d24c1446028b3fe441c5
Emma <alunia.moon@gmail.com>
2025-01-29 11:59:15 +0100

    Refactor JS to only have one API call
    I got stuck spinning around on errors only to find out it was because of the way I changed my fetch functionality, and I was calling it with a url parameter that it  didn't have! I just needed to stop calling it on page load.

--- script.js
+++ script.js
@@ -1,53 +1,28 @@
-const ABILITY_SCORES_URL = "https://www.dnd5eapi.co/api/ability-scores";
 
 // fetch API data
-const fetchApiData = async () => {
-    try {
-        const response = await fetch(ABILITY_SCORES_URL);
-        if (!response.ok) {
-            throw new Error("Epic fail");
-        }
-        const data = await response.json();
-        // adds count to track nr of each stat the user has picked
-        data.results.forEach(stat => {
-            stat.count = 0;
-        });
+// const fetchStatDetails = async () => {
+//     try {
+//         const response = await fetch(ABILITY_SCORES_URL);
+//         if (!response.ok) {
+//             throw new Error("Epic fail");
+//         }
+//         const data = await response.json();
+//         // adds count to track nr of each stat the user has picked
+//         data.results.forEach(stat => {
+//             stat.count = 0;
+//         });
 
-        // console.log(data.results)
-        // save in localstorage
-        localStorage.setItem("character_stats", JSON.stringify(data));
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
-
-};
-
-// check if data stored in local storage
-const checkCurrentData = async () => {
-    const character_stats = JSON.parse(localStorage.getItem("character_stats"));
-    if (character_stats) {
-        console.log("There are characters saved");
-        // renderCharacterStats(character_stats);
-    } else {
-        console.log("No characters yet");
-        const data = await fetchApiData();
-        // console.log(data);
-    };
-};
+//         // console.log(data.results)
+//         // save in localstorage
+//         localStorage.setItem("character_data", JSON.stringify(data));
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+// 
 
 
-/* 
-add stats to array 
-add max stats
-deduct available stats from max stats when a number of stats is chosen
-
-*/
-
-const applyingStatChoices = async () => {
-    const maxStats = 20;
-
-}
 
 // new API call to use for "read more about this stat" https://www.dnd5eapi.co/api/ability-scores/${url}
 
@@ -58,7 +33,13 @@ const fetchStatDetails = async (url) => {
             throw new Error("Failed to fetch stat description");
         }
         const data = await response.json();
-        console.log(data.desc)
+        // data.results.forEach(stat => {
+        //     stat.count = 0;
+        // });
+        // save in localstorage
+        localStorage.setItem("character_data", JSON.stringify(data));
+        console.log("Button click:", data.desc);
+        return data;
     } catch (error) {
         console.error(error);
     }
@@ -71,9 +52,38 @@ statsContainer.addEventListener("click", (e) => {
         const url = e.target.dataset.url; // dataset is the data-url tag on the HTML elements
         fetchStatDetails(`https://www.dnd5eapi.co${url}`)
     }
-})
+});
+
+// check if data stored in local storage
+const checkCurrentData = async () => {
+    const character_data = JSON.parse(localStorage.getItem("character_data"));
+    if (character_data) {
+        console.log("There are characters saved");
+        renderCharacterData(character_data);
+    } else {
+        console.log("No characters yet");
+        const data = await fetchStatDetails();
+        console.log(data);
+    }
+};
 
 
 
-fetchApiData();
+
+
+// save character data
+const saveCharacter = () => {
+    // collect stat name and value
+    // store data in obj or array
+}
+
+// render any data found in localstorage
+
+const renderCharacterData = (character) => {
+    // currently targetting the ability scores rather than `https://www.dnd5eapi.co${url}`because of my first fetch fn
+    console.log("character data: ", character)
+};
+
+
+// fetchStatDetails();
 checkCurrentData();
